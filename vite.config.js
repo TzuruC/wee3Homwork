dec1a3399af47af33b378f1317720241ca6c7faa
TzuruCh <tzuruc@gmail.com>
2023-08-02 12:47:14 +0800

    change repo name

--- vite.config.js
+++ vite.config.js
@@ -25,7 +25,7 @@ function moveOutputPlugin() {
 export default defineConfig({
   // base 的寫法：
   // base: '/Repository 的名稱/'
-  base: '/week3-homework/',
+  base: '/week3Homwork/',
   plugins: [
     liveReload(['./layout/**/*.ejs', './pages/**/*.ejs', './pages/**/*.html']),
     ViteEjsPlugin(),
